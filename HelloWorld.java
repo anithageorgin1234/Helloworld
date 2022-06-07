620528643ff013270ffb999e40a58f5854046061
anithageorgin <devlead@rlard.com>
2022-06-07 13:00:04 +0530

    ch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
-public class Helloworld{
-        public static void main(String[] args)
-        {
-                 System.out.println(â€œHello, Worldâ€);
-        }
-        }
+public class HelloWorld {
+	public static void main(String args[])
+	{
+		System.out.println("My Name is Anitha Georgin");
+		System.out.println("I am 33 Years Old");
+		System.out.println("My weight is 65 ");
+		System.out.println("#74/B Shalom Bahavn,Kottyam");
+	}
+
+}
